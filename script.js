70661ca182471a7c2375096014a3a5d346d443e7
bjvaldiviezo <bjvaldiviezo@gmail.com>
2022-07-25 13:57:19 -0300

    clase05 Completada

--- script.js
+++ script.js
@@ -3,14 +3,24 @@ import deleteIcon from './components/deleteIcon.js';
 
 const btn = document.querySelector("[data-form-btn]");
 //Arrows Function
+const addTask = (evento) =>{
+  const list = document.querySelector("[data-list]");
+  const task = createTask(evento);
+  list.appendChild(task);
+}
+
 const createTask = (evento) => {
   evento.preventDefault();
   const input = document.querySelector("[data-form-input]");
-  const list = document.querySelector("[data-list]");
+  const calendar = document.querySelector("[data-form-date]")
+  const date = calendar.value;
+  const dateFormat = moment(date).format('DD/MM/YYYY')
+  console.log(dateFormat);
   const task = document.createElement("li");
   const value = input.value;
   task.classList.add('card');
   input.value = '';
+
 
   //Backticks `` alt gr + }
   const taskContent = document.createElement("div");
@@ -19,12 +29,15 @@ const createTask = (evento) => {
   titleTask.classList.add("task");
   titleTask.innerText = value
   taskContent.appendChild(titleTask);
-  //task.innerHTML = content;
+
+  const dateElement = document.createElement("span");
+  dateElement.innerHTML = dateFormat;
   task.appendChild(taskContent);
+  task.appendChild(dateElement)
   task.appendChild(deleteIcon());
-  list.appendChild(task);
+  return task;
 }
 
 
 //Arrow Functions o funciones anonimas
-btn.addEventListener("click", createTask);
+btn.addEventListener("click", addTask);
