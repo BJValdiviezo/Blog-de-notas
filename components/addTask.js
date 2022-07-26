8d1af5e584a3060d3e39f10a42b4c76a2c27167e
bjvaldiviezo <bjvaldiviezo@gmail.com>
2022-07-26 14:07:32 -0300

    Clase06 Completada

--- components/addTask.js
+++ components/addTask.js
@@ -1,49 +1,51 @@
-import {checkComplete} from './checkComplete.js';
-import {deleteIcon} from './deleteIcon.js';
+import { checkComplete } from './checkComplete.js';
+import { deleteIcon } from './deleteIcon.js';
 
 export const addTask = (evento) => {
-    const list = document.querySelector("[data-list]");
-    const task = createTask(evento);
-    list.appendChild(task);
-}
-  
-  const createTask = (evento) => {
-    evento.preventDefault();
-    const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
-    console.log(taskList);
+  evento.preventDefault();
 
-    const input = document.querySelector("[data-form-input]");
-    const calendar = document.querySelector("[data-form-date]")
-    const date = calendar.value;
-    const dateFormat = moment(date).format('DD/MM/YYYY')
-    console.log(dateFormat);
-    const task = document.createElement("li");
-    const value = input.value;
-    task.classList.add('card');
-    input.value = '';
-  
-  
-    //Backticks `` alt gr + }
-  
-    const taskContent = document.createElement("div");
-    console.log(value, dateFormat);
-    const taskObj = {
-      value,
-      dateFormat
-    }
-    taskList.push(taskObj)
-    localStorage.setItem('tasks',JSON.stringify(taskList));
+  const list = document.querySelector("[data-list]");
+  const input = document.querySelector("[data-form-input]");
+  const calendar = document.querySelector("[data-form-date]");
 
-    taskContent.appendChild(checkComplete());
-    const titleTask = document.createElement("span");
-    titleTask.classList.add("task");
-    titleTask.innerText = value
-    taskContent.appendChild(titleTask);
+  const value = input.value;
+  const date = calendar.value;
+  const dateFormat = moment(date).format('DD/MM/YYYY');
   
-    const dateElement = document.createElement("span");
-    dateElement.innerHTML = dateFormat;
-    task.appendChild(taskContent);
-    task.appendChild(dateElement)
-    task.appendChild(deleteIcon());
-    return task;
+  const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
+
+
+  input.value = '';
+  calendar.value = '';
+
+  const taskObj = {
+    value,
+    dateFormat
   }
+  taskList.push(taskObj);
+  localStorage.setItem('tasks', JSON.stringify(taskList));
+  
+  const task = createTask(taskObj);
+  list.appendChild(task);
+}
+
+const createTask = ({ value, dateFormat }) => {
+
+  const task = document.createElement("li");
+  task.classList.add('card');
+  //Backticks `` alt gr + }
+  const taskContent = document.createElement("div");
+  console.log(value, dateFormat);
+
+  taskContent.appendChild(checkComplete());
+  const titleTask = document.createElement("span");
+  titleTask.classList.add("task");
+  titleTask.innerText = value
+  taskContent.appendChild(titleTask);
+  const dateElement = document.createElement("span");
+  dateElement.innerHTML = dateFormat;
+  task.appendChild(taskContent);
+  task.appendChild(dateElement)
+  task.appendChild(deleteIcon());
+  return task;
+}
