1c8181ae728b232ae2bcc2d57957a48b9f8c399a
bjvaldiviezo <bjvaldiviezo@gmail.com>
2022-07-26 12:30:08 -0300

    Clase 06 Completada

--- components/checkComplete.js
+++ components/checkComplete.js
@@ -1,4 +1,4 @@
-const checkComplete = () => {
+export const checkComplete = () => {
     const i = document.createElement("i");
     i.classList.add("far", "fa-check-square", "icon");
     i.addEventListener("click", completeTask);
--- components/deleteIcon.js
+++ components/deleteIcon.js
@@ -1,4 +1,4 @@
-const deleteIcon = () => {
+export const deleteIcon = () => {
     const i = document.createElement("i");
     i.classList.add("fas", "fa-trash-alt", "trashIcon", "icon");
     i.addEventListener("click", deleteTask);
